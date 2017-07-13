7fc4f234c4260608bf4623d898d22509c8e8dbe6
Electroid <electroidfilms@gmail.com>
2017-07-13 01:45:03 -0700

    More git changes

--- script/git.sh
+++ script/git.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 
-# Clone the repository from scratch
-rm -rf data && git clone -b $GIT_BRANCH $GIT_URL data && cd data
+# Clone the repository
+git clone -b $GIT_BRANCH $GIT_URL data
+cd data
+
+# Ensure repository is in correct state (variables may have changed)
+git reset --hard origin/$GIT_BRANCH && git pull
 
 # Wait a bit and always run the command on start
-sleep $GIT_TIME && $GIT_CMD && echo "Waiting..."
+sleep $GIT_TIME && $GIT_CMD && echo "Listening..."
 
 # If any changes were made to the repository, pull and run the command
 while true; do
   git fetch origin
   log=$(git log HEAD..origin/$GIT_BRANCH --oneline)
   if [[ "${log}" != "" ]] ; then
-    echo "Updating..." && git merge origin/$GIT_BRANCH && $GIT_CMD
+    git merge origin/$GIT_BRANCH && $GIT_CMD
+    echo "Listening..."
   fi
   sleep $GIT_TIME
 done
