278c2bdc49d29e882722a42dad576585d20d19f1
Electroid <electroidfilms@gmail.com>
2017-07-13 01:24:47 -0700

    Change git script again

--- script/git.sh
+++ script/git.sh
@@ -6,10 +6,12 @@ rm -rf data && git clone -b $GIT_BRANCH $GIT_URL data && cd data
 # Wait a bit and always run the command on start
 sleep $GIT_TIME && $GIT_CMD && echo "Waiting..."
 
+# If any changes were made to the repository, pull and run the command
 while true; do
-  # If any changes were made to the repository, pull and run the command
-  if ! git diff --quiet remotes/origin/HEAD; then
-    echo "Updating..." && git reset --hard origin/$GIT_BRANCH && git pull && $GIT_CMD
+  git fetch origin
+  log=$(git log HEAD..origin/$GIT_BRANCH --oneline)
+  if [[ "${log}" != "" ]] ; then
+    echo "Updating..." && git merge origin/$GIT_BRANCH && $GIT_CMD
   fi
   sleep $GIT_TIME
 done
