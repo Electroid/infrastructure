23d800a7ad4662643298a57d068d5c799257a2b5
Electroid <electroidfilms@gmail.com>
2017-12-05 15:50:40 -0700

    Fix folder location

--- minecraft/cloudy/start-cloudy.sh
+++ minecraft/cloudy/start-cloudy.sh
@@ -8,5 +8,9 @@ cp -R /minecraft/server-new/* /minecraft/server
 cd /minecraft/server
 rm -rf /minecraft/server-new
 
+# Ensure new api-ocn config is copied
+rm /minecraft/server/plugins/API-OCN/config.yml 
+cp /minecraft/server/plugins/API/config.yml /minecraft/server/plugins/API-OCN/config.yml 
+
 # Start the cloudy server
 exec ./start-bukkit.sh
