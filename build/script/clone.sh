6272c6b45d8bf55c522c29e44b1dadd4572fe4ab
Electroid <electroidfilms@gmail.com>
2017-12-05 15:03:03 -0700

    Updates to Minecraft build structure, again

--- build/script/clone.sh
+++ build/script/clone.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-# Usage: ./clone.sh {GitUser} {GitRepoName} {GitRepoBranch} [Command]
 
+# Clone a repository and run a command
 git clone -b $3 -- https://github.com/$1/$2.git $2
 cd $2
 ${4:-mvn -T 1C -Dmaven.test.skip=true install -am}
