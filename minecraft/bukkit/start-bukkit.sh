d3553f12019e1ab3da4e0b4418a46796db410764
Electroid <electroidfilms@gmail.com>
2017-12-22 23:46:16 -0700

    Make some improvements to the shared start script

--- minecraft/bukkit/start-bukkit.sh
+++ minecraft/bukkit/start-bukkit.sh
@@ -7,20 +7,4 @@ find . -name "*.yml" -type f -exec sh -c "envsubst < {} > env && rm {} && mv env
 find . -name "*.properties" -type f -exec sh -c "envsubst < {} > env && rm {} && mv env {}" \;
 sleep 2 && mv commands.ignore commands.yml &
 
-# Add default map if not connected to maps repo
-if [ ! -d "/minecraft/maps" ]; then
-  mkdir -p /minecraft/maps
-  cp -R world /minecraft/maps/map
-fi
-
-# Also check to see if the folder is just empty
-if [ -z "$(ls -A /minecraft/maps)" ]; then
-  cp -R world /minecraft/maps/map
-fi
-
-# Make sure dropbox folder exists even if not enabled
-if [ ! -d "/minecraft/dropbox" ]; then
-  mkdir -p /minecraft/dropbox
-fi
-
 exec ./start.sh
--- minecraft/shared/start.sh
+++ minecraft/shared/start.sh
@@ -3,30 +3,6 @@
 echo "Starting $SERVER_ROLE server in $SERVER_STAGE..."
 cd /minecraft/server
 
-echo "Performing role specific commands..."
-if [[ "$SERVER_ROLE" == "PGM" ]]; then
-	rm plugins/lobby.jar
-	if [ -z "$SERVER_TOURNAMENT_ID" ]; then
-		rm plugins/tourney.jar
-	fi
-elif [[ "$SERVER_ROLE" == "LOBBY" ]]; then
-	rm plugins/pgm.jar
-	if [ -d "/minecraft/maps/Lobby" ]; then
-		rm -rf world
-  		cp -R /minecraft/maps/Lobby world
-	fi
-elif [[ "$SERVER_ROLE" == "BUNGEE" ]]; then
-	echo ""
-else
-	rm plugins/lobby.jar
-	rm plugins/pgm.jar
-	rm plugins/tourney.jar
-fi
-
-if [[ "$SENTRY_DSN_PLUGINS" == "null" ]]; then
-	rm plugins/raven.jar
-fi
-
 if [ -z "$SERVER_IDS" ]; then
 	if [[ "$SERVER_ID" == "null" ]]; then
 		echo "No server id specified... standalone mode"
@@ -42,24 +18,76 @@ else
 		echo "Selected list server id at index $SERVER_INDEX... $SERVER_ID"
 	else
 		for id in "${SERVER_IDS_ARRAY[@]}"; do
-			if [[ $(curl -s http://$SERVER_API_IP/servers/$id | jq .online) != "true" ]]; then
+			if [[ $(curl -m 5 -s http://$SERVER_API_IP/servers/$id | jq .online) == "false" ]]; then
 				export SERVER_ID="$id"
 				echo "Selected set server id... $SERVER_ID"
 				break
 			fi
 		done
-		if [ -z "$SERVER_ID" ]; then
+		if [[ "$SERVER_ID" == "null" ]]; then
 			echo "Could not find a possible server id for hostname... $HOSTNAME" && exit 1
 		fi
 	fi
 fi
 
-echo "Syncing both api configurations..."
+if [ -e plugins/api-ocn.jar ]; then
+	echo "Authenticating the server id with api..."
+	for i in {1..5}; do
+		export SERVER_DOCUMENT=$(curl -m 10 -s http://$SERVER_API_IP/servers/$SERVER_ID)
+		if [ ! -z "$SERVER_DOCUMENT" ]; then
+			break
+		else
+			echo " > Retry $i..."
+			sleep 10
+		fi
+		echo "Unable to fetch server document with id... $SERVER_ID" && exit 1
+	done
+	export SERVER_NAME=$(echo $SERVER_DOCUMENT | jq .name)
+	echo "Received server identity as $SERVER_NAME..."
+else
+	export SERVER_NAME="default"
+fi
+
+echo "Performing role specific commands..."
+if [[ "$SERVER_ROLE" == "PGM" ]]; then
+	rm plugins/lobby.jar
+	if [[ "$SERVER_SENTRY_DSN" == "null" ]]; then
+		rm plugins/tourney.jar
+	fi
+	if [[ "$SERVER_ROTATION" == "null" ]]; then
+		export SERVER_ROTATION=$SERVER_NAME
+	fi
+	sed -i -e "s/!SERVER_ROTATION/${SERVER_ROTATION}/g" plugins/PGM/config.yml
+	mkdir -p /minecraft/maps
+	if [ -z "$(ls -A /minecraft/maps)" ]; then
+		echo "Using fallback map since no repository was provided..."
+		cp -R world /minecraft/maps/map
+	fi
+elif [[ "$SERVER_ROLE" == "LOBBY" ]]; then
+	rm plugins/pgm.jar
+	rm plugins/tourney.jar
+	if [ -d "/minecraft/maps/Lobby" ]; then
+		rm -rf world
+		cp -R /minecraft/maps/Lobby world
+	fi
+elif [[ "$SERVER_ROLE" == "MAPDEV" ]]; then
+	rm plugins/lobby.jar
+	rm plugins/pgm.jar
+	rm plugins/tourney.jar
+fi
+
+if [[ "$SERVER_SENTRY_DSN" == "null" ]]; then
+	rm plugins/raven.jar
+fi
+
+if [[ "$SERVER_BUYCRAFT" == "null" ]]; then
+	rm plugins/buycraft.jar
+fi
+
+echo "Injecting environment variables in configuration files..."
 sed -i -e "s/!SERVER_ID/${SERVER_ID}/g" health.sh
 sed -i -e "s/!SERVER_ID/${SERVER_ID}/g" plugins/API/config.yml
 cp -R plugins/API plugins/API-OCN
-
-echo "Injecting environment variables in configuration files..."
 for query in '*.yml' '*.yaml' '*.json' '*.properties'; do
 	find . -name $query -type f -exec sh -c "echo ' > {}' && envsubst < {} > env && rm {} && mv env {}" \;
 done
