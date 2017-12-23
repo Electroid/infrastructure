129a12a03694f3206da17f999127527ded4573a4
Electroid <electroidfilms@gmail.com>
2017-12-22 23:45:58 -0700

    Change health script to also work for bukkit servers

--- minecraft/shared/health.sh
+++ minecraft/shared/health.sh
@@ -4,9 +4,13 @@ if [[ "$SERVER_LOCAL" == "true" ]]; then
 	echo "ok" && exit 0
 fi
 
-status=`curl -s http://$SERVER_API_IP/servers/!SERVER_ID | jq .dns_enabled`
-if [[ "$status" == "true" ]]; then
-	echo "ok" && exit 0
-elif [[ "$status" == "false" ]]; then
-	echo "nok" && exit 1
+if [[ "$SERVER_ROLE" == "BUNGEE" ]]; then
+	status=$(curl -s http://$SERVER_API_IP/servers/!SERVER_ID | jq .dns_enabled)
+	if [[ "$status" == "true" ]]; then
+		echo "ok" && exit 0
+	elif [[ "$status" == "false" ]]; then
+		echo "nok" && exit 1
+	fi
+else
+	mcstatus localhost:$SERVER_PORT ping
 fi
