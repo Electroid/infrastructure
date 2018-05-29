a1890159605b2f284ed5c49adb29cd7bee3dcee6
Ashcon Partovi <ashcon@partovi.net>
2018-05-28 18:08:27 -0700

    Change google authentication to be via environment variables

--- lib/google/api.rb
+++ lib/google/api.rb
@@ -1,5 +1,6 @@
 require "googleauth"
 require "google/apis/compute_beta"
+require "environment"
 
 # Represents an object that interacts with any Google API.
 module Google
@@ -11,13 +12,14 @@ module Google
 
     # Get the project ID that is specified in the credentials file.
     def project_id
-        @project_id ||= JSON.parse(File.read(credentials))["project_id"]
+        @project_id ||= Env.need("google_project_id")
     end
 
     # Authenticate a Google service object with a path to a JSON credentials file.
+    # The environment variables 'GOOGLE_PRIVATE_KEY' and 'CLIENT_EMAIL_VAR' must
+    # be defined for authentication to work properly.
     def auth(service)
         service.authorization = Google::Auth::ServiceAccountCredentials.make_creds({
-            :json_key_io => File.open(credentials, "r"),
             :scope => "https://www.googleapis.com/auth/compute"
         })
         service
