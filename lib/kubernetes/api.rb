6586fff2e22242b211891068f347384d2059a43a
Ashcon Partovi <ashcon@partovi.net>
2018-05-28 17:54:16 -0700

    Allow programs to access cluster from inside pod

--- lib/kubernetes/api.rb
+++ lib/kubernetes/api.rb
@@ -4,13 +4,46 @@ require "celluloid/io"
 # Represents an object that interacts with the Kubernetes cluster. 
 module Kubernetes
     def cluster
-        @config ||= Kubeclient::Config.read(File.expand_path("~/.kube/config"))
-        unless @client
-            context = @config.context
-            ssl_options = context.ssl_options
-            ssl_options[:verify_ssl] = 0
+        @cluster ||= begin
+            cluster_internal
+        rescue
+            cluster_external
         end
-        @client ||= Kubeclient::Client.new(
+    end
+
+    def nodes
+        cluster.get_nodes.map{|node| Node.new(node.metadata.name)}
+    end
+
+    protected
+
+    # Access the cluster from inside a pod that has a service account.
+    def cluster_internal
+        Kubeclient::Client.new(
+            "https://kubernetes.default.svc",
+            "v1",
+            {
+                ssl_options: {
+                    ca_file: "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+                },
+                auth_options: {
+                    bearer_token_file: "/var/run/secrets/kubernetes.io/serviceaccount/token"
+                },
+                socket_options: {
+                    socket_class: Celluloid::IO::TCPSocket,
+                    ssl_socket_class: Celluloid::IO::SSLSocket
+                }
+            }
+        )   
+    end
+
+    # Access the cluster from an external machine.
+    def cluster_external
+        config = Kubeclient::Config.read(File.expand_path("~/.kube/config"))
+        context = config.context
+        ssl_options = context.ssl_options
+        ssl_options[:verify_ssl] = 0
+        Kubeclient::Client.new(
             context.api_endpoint,
             context.api_version,
             {
@@ -22,9 +55,5 @@ module Kubernetes
                 }
             }
         )
-    end
-
-    def nodes
-        cluster.get_nodes.map{|node| Node.new(node.metadata.name)}
     end
 end
