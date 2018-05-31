1c1b0724e04d337d3ed793ef006e99cf2c5776fd
Ashcon Partovi <ashcon@partovi.net>
2018-05-31 01:27:12 -0700

    Fix invalid git states in repo worker

--- lib/worker/repo.rb
+++ lib/worker/repo.rb
@@ -10,7 +10,7 @@ class RepoWorker < Worker
         repo:   nil,      # Git repo (ie. Electroid/infrastructure)
         branch: "master", # Git branch
         dir:    "data",   # Git directory
-        hook:   "pwd"     # Hook command  (ie. curl http://example.com/webhook)
+        hook:   "pwd"     # Hook command (ie. curl http://example.com/webhook)
     )
 
     def initialize(repo, branch, path, hook=nil)
@@ -45,27 +45,30 @@ class RepoWorker < Worker
 
     # Pull the latest changes from the remote branch.
     def pull
+        execute("git fetch --depth 1") &&
         execute("git reset --hard origin/#{@branch}") &&
-        execute("git fetch --update-head-ok origin #{@branch}:#{@branch} --depth 1")
+        execute("git clean -dfx")
     end
 
     # Initially clone or fix the repository before pull.
     def clone
         FileUtils.mkdir_p(@parent_path)
         if Dir.exist?(@path)
-            if !Dir.exist?("#{@path}/.git")
+            if !Dir.exist?(File.join(@path, ".git"))
                 log("Removing empty repository")
             elsif @uri != (uri = %x(cd #{@path} && git config --get remote.origin.url).strip)
-                log("Removing another repository connected to #{uri.spit("/").last.split(".").first}")
+                log("Removing another repository: #{uri.spit("/").last.split(".").first}")
+            elsif @branch != (branch = %(cd #{@path} && git symbolic-ref --short -q HEAD).strip)
+                log("Removing another branch: #{branch}")
             else
                 valid = true
-                log("Found valid repository for #{@name}")
+                log("Found valid repository: #{@name}")
             end
             FileUtils.rm_rf(@path) unless valid
         elsif execute("git clone --single-branch --depth 1 -b #{@branch} #{@uri} #{@path}")
-            log("Cloned initial repository for #{@name}")
+            log("Cloned initial repository: #{@name}")
         else
-            log("Failed to clone initial repository at #{@uri}")
+            log("Failed to clone initial repository: #{@uri}")
             exit(1)
         end
         Dir.chdir(@path)
@@ -74,7 +77,7 @@ class RepoWorker < Worker
     # Execute a shell command or exit the program if a failure occurs.
     def execute(cmd, fails=true)
         unless system("#{cmd}", :out => File::NULL)
-            log("Error executing shell command '#{cmd}'")
+            log("Error executing shell command: '#{cmd}'")
             exit(1) if fails
         end
         true
